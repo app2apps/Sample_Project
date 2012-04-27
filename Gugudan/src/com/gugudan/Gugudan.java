d1b17ede5e9490100b79273d7f157ddb57a1358f
henawang <henawang@192.168.123.193>
2012-04-28 01:18:19 +0900

    test2

--- Gugudan/src/com/gugudan/Gugudan.java
+++ Gugudan/src/com/gugudan/Gugudan.java
@@ -9,7 +9,6 @@ public class Gugudan {
 		// TODO Auto-generated method stub
 		System.out.println("test");
 		
-		
 		for(int i=2; i<=9; i++) {
 			for(int j=1; j<=9; j++) {
 				System.out.println(i + " x " + j + " = " + i*j );
